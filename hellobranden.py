d7690aa1e1014ed9257ac689e8a084aa00de7995
BrandenBent <brandenmichaelbent@gmail.com>
2024-09-30 14:39:28 +0900

    requirements.txt for python environments

--- hellobranden.py
+++ hellobranden.py
@@ -1,3 +1,14 @@
+from flask import Flask
+from datetime import datetime
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    unset_jwt_cookies,
+    jwt_required,
+    get_jwt_identity,
+)
+
+
 def hello():
     print("Hello World")
 
